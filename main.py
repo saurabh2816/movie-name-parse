eb097c7ef55d6f998aecc2d323022c90c44ea4f0
saurabh <saurabh2816@gmail.com>
2023-04-07 16:38:41 -0700

    fixing deploy

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-# Using flask to make an api
-# import necessary libraries and functions
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+import os
+
 import PTN
 
-# creating a Flask app
 app = Flask(__name__)
 
 
-# on the terminal type: curl http://127.0.0.1:5000/
-# returns hello world when we use GET.
-# returns the data that we send when we use POST.
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if (request.method == 'GET'):
-        data = "hello world"
-        return jsonify({'data': PTN.parse("10.Days.with.Dad.2020.480p.Farsi.Subbed.HivaMovie.mkv")})
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo":PTN.parse("10.Days.with.Dad.2020.480p.Farsi.Subbed.HivaMovie.mkv")})
 
 
-# driver function
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
