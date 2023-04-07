0258bf11f02055c038a8c5623890a5cf5e60a019
saurabh <saurabh2816@gmail.com>
2023-04-07 16:30:39 -0700

    adding main files

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
-import os
+# Using flask to make an api
+# import necessary libraries and functions
+from flask import Flask, jsonify, request
+import PTN
 
+# creating a Flask app
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if (request.method == 'GET'):
+        data = "hello world"
+        return jsonify({'data': PTN.parse("10.Days.with.Dad.2020.480p.Farsi.Subbed.HivaMovie.mkv")})
 
 
+# driver function
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
